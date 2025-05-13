f36b495293402e957045aeead4be3756a556f088
anouarohs <anouar.ohs@gmail.com>
2025-05-13 23:01:39 +0000

    Correction de la référence à src/index.js pour utiliser src/index.jsx

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./src/index.jsx", // Spécifie explicitement index.jsx comme point d'entrée
+      },
+    },
+  },
 });
